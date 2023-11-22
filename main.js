2f8453ca5a9ff6d383fba2266649c7dc70e27265
Chris Felix <chrisfel@ca.ibm.com>
2023-11-21 21:57:44 -0500

    updates open api spec

--- main.js
+++ main.js
@@ -14,12 +14,13 @@ var schema = buildSchema(`
 `)
 
 
+async function findVehicles(searchInput) {
+  return {text: `No cars found for your query ${searchInput}`}
+}
 
 // The root provides a resolver function for each API endpoint
 var root = {
-  findVehicles: (params) => {
-    return {text: `No cars found for your query ${params.searchInput}`}
-  },
+  findVehicles: findVehicles
 }
 
 var app = express()
@@ -31,5 +32,19 @@ app.use(
     graphiql: true,
   })
 )
+
+app.get("/example-service/query", (req, res) => {
+ findVehicles(req.query.query)
+  .then((text) => {
+    res.send(JSON.stringify({
+      data: {
+        findVehicles: text
+      }
+    }))
+  })
+  .catch((err) => {
+    res.send(err)
+  })
+})
 app.listen(4000)
 console.log("Running a GraphQL API server at http://localhost:4000/graphql")
